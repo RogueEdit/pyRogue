68f815039d136a422d58d433d65a0daa663514d9
stblr <68881884+JulianStiebler@users.noreply.github.com>
2024-06-01 18:27:15 +0200

    Update loginLogic.py

--- src/modules/loginLogic.py
+++ src/modules/loginLogic.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-class LoginLogic:
+class loginLogic:
     LOGIN_URL = "https://api.pokerogue.net/account/login"
 
     def __init__(self, username: str, password: str) -> None:
@@ -15,12 +15,25 @@ class LoginLogic:
 
     def login(self) -> bool:
         data = {"username": self.username, "password": self.password}
+        headers = {
+            "Accept": "application/x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Referer": "https://pokerogue.net/",
+            "Sec-Ch-Ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        }
         try:
-            response = self.session.post(self.LOGIN_URL, json=data)
+            print("Attempting login...")
+            response = self.session.post(self.LOGIN_URL, json=data, headers=headers)
+            print("Login request sent.")
             response.raise_for_status()
+            print("Received response from server.")
             self.token = response.json().get("token")
+            print("Token received:", self.token)
             self.auth_headers = {
-                "Authorization": f"Bearer {self.token}",
+                "authorization": self.token,
                 "Accept": "application/x-www-form-urlencoded",
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Referer": "https://pokerogue.net/",
