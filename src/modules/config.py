b4da90316cfba20599d5c442461e86750802a369
stblr <68881884+JulianStiebler@users.noreply.github.com>
2024-06-20 05:42:06 +0200

    Update config.py

--- src/modules/config.py
+++ src/modules/config.py
@@ -7,7 +7,7 @@ version = 'v0.2.3'
 owner = 'rogueEdit'
 repo = 'onlineRogueEditor'
 repo_url = f'https://github.com/{owner}/{repo}/'
-release_date = '20.06.2024 3:58'
+release_date = '20.06.2024 5:43'
 
 
 
